90c2e4971683b0c1b68411fc1b6a09431226c3ef
Illia Kravchenko <117378994+ilkravchenko@users.noreply.github.com>
2023-04-26 11:07:26 +0300

    Upload new project where created letters for friends using automatization for working with files and paths

---
+++ Mail_Merge_Project/main.py
@@ -0,0 +1,20 @@
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Letters/starting_letter.txt") as file:
+    letter = file.read()
+
+with open("./Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+
+for name in names:
+    name = name.strip()
+    with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode='w') as file:
+        result_letter = letter.replace('[name]', f'{name}')
+        file.write(result_letter)
