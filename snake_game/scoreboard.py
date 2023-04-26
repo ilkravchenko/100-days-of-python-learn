629dd28550136831fb4ce385d851e7d3a14c8caa
Illia Kravchenko <117378994+ilkravchenko@users.noreply.github.com>
2023-04-26 10:22:48 +0300

    Add tracking for high score

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -3,19 +3,32 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ('Arial', 16, 'normal')
 
+
 class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open("data.txt") as file:
+            content = file.read()
+            self.high_score = int(content)
         self.score = 0
         self.color('white')
         self.penup()
         self.goto(0, 278)
         self.hideturtle()
-        self.write_score()
+        self.update_scoreboard()
 
-    def write_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}. High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', 'w') as file:
+                file.write(str(self.score))
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0, 0)
@@ -23,5 +36,4 @@ class ScoreBoard(Turtle):
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write_score()
+        self.update_scoreboard()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -25,6 +25,13 @@ class Snake:
         new_segment.goto(possition)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for segm in self.segments:
+            segm.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
