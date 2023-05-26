37c2da01629fc4978615cf20ad43eef3be5ac94a
Illia Kravchenko <117378994+ilkravchenko@users.noreply.github.com>
2023-05-26 16:57:39 +0300

    Updated learning about paths and decorators

--- Hello_flask/hello.py
+++ Hello_flask/hello.py
@@ -1,9 +1,56 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
+def make_bold(func):
+    def wrapper_func():
+        result = func()
+        return f"<b>{result}</b>"
+
+    return wrapper_func
+
+
+def make_emphasis(func):
+    def wrapper_func():
+        result = func()
+        return f"<em>{result}</em>"
+
+    return wrapper_func
+
+
+def make_underlined(func):
+    def wrapper_func():
+        result = func()
+        return f"<u>{result}</u>"
+
+    return wrapper_func
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align:center">Hello, World!</h1>' \
+           '<p>This is paragraph</p>' \
+           '<img sourse="https://www.thedailyworld.com/wp-content/uploads/2020/05/21591430_web1_KittenRescue-ADW-200520-kitten_2.jpg">'
+
+
+@app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
+def bye():
+    return 'Bye!'
+
+
+@app.route('/username/<name>')
+def greet(name):
+    return f'Hello {name}!'
+
+
+@app.route('/username/<name>/<int:number>')
+def greet_age(name, number):
+    return f'Hello {name}! You are {number} years old?'
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- Hello_flask/learn_decorator.py
+++ Hello_flask/learn_decorator.py
@@ -22,3 +22,45 @@ def slow_function():
 
 fast_function()
 slow_function()
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authentication_decorator(func):
+    def wrapper_func(*args, **kwargs):
+        if args[0].is_logged_in:
+            func(args[0])
+    return wrapper_func
+
+@is_authentication_decorator
+def create_blog_post(user:User):
+    print(f"This is {user.name}'s new blog post")
+
+new_user = User('Illia')
+create_blog_post(new_user)
+
+
+# Create the logging_decorator() function 👇
+def logging_decorator(func):
+    def wrapper_func(*args):
+        result = ''
+
+        calculate = func(*args)
+
+        result += f'You called {func.__name__}{args}\n'
+        result += f'It returned: {calculate}'
+
+        return result
+
+    return wrapper_func
+
+
+# Use the decorator 👇
+@logging_decorator
+def add_nums(*args):
+    return sum(args)
+
+
+print(add_nums(1, 2, 3))
