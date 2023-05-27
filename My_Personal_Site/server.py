95af382dc23fac2b106a695c0e53686c30a83791
Illia Kravchenko <117378994+ilkravchenko@users.noreply.github.com>
2023-05-27 11:32:01 +0300

    Upload Personal site created using Flask and using Templates from Internet

---
+++ My_Personal_Site/server.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/card')
+def card():
+    return render_template('card.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
